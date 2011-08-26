3e1a8d060e132bbdf1abdda2401f31908d769946
Rik van der Kemp <rik@zicht.nl>
2011-08-26 15:23:43 +0000

    git-svn-id: svn://zon/standards/php/trunk@10 7ddae273-7408-4bef-b35c-908d3e90a928

--- Zicht/Sniffs/ControlStructures/ControlSignatureSniff.php
+++ Zicht/Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -15,13 +15,13 @@ class Zicht_Sniffs_ControlStructures_ControlSignatureSniff extends PHP_CodeSniff
      */
     protected function getPatterns() {
         return array(
-            'do {EOL...} while(...);EOL',
-            'while(...) {EOL',
-            'for(...) {EOL',
-            'if(...) {EOL',
-            'foreach(...) {EOL',
-            '} else if(...) {EOL',
-            '} elseif(...) {EOL',
+            'do {EOL...} while (...);EOL',
+            'while (...) {EOL',
+            'for (...) {EOL',
+            'if (...) {EOL',
+            'foreach (...) {EOL',
+            '} else if (...) {EOL',
+            '} elseif (...) {EOL',
             '} else {EOL',
             'do {EOL',
         );
